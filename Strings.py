8f9a20515f34d8b864a21cf5614cf2470215f174
Catalin <stirbuc@icloud.com>
2023-02-13 17:56:34 +0200

    Monday Coding Added a new file named "Strings":     Added Examples and an explanation

---
+++ Strings.py
@@ -0,0 +1,16 @@
+# Example 1
+
+word = 'by'
+print(len(word))
+
+# Example 2
+
+empty = ''
+print(len(empty))
+
+# Example 3
+# Don't forget that a backslash (\) used as an escape character is not included in the string's total length.
+# The code in Example 3, therefore 0outputs 3.
+
+i_am = 'I\'m'
+print(len(i_am))
