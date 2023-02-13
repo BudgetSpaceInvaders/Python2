2e18d65f9703987e7c7a9f956ec1496f304a55bb
Catalin <stirbuc@icloud.com>
2023-02-13 18:27:37 +0200

    Monday Coding Added a new file named "Strings":     Added more examples and more explanations

--- Strings.py
+++ Strings.py
@@ -1,4 +1,5 @@
-# Example 1
+"""
+# # Example 1
 
 word = 'by'
 print(len(word))
@@ -23,3 +24,37 @@ print(str2 + str1)
 #The asterisk makes the string be replicated as many times as the number it is multiplied with
 print(5 * 'a')
 print('b' * 4)
+
+# Demonstrating the ord() function.
+
+char_1 = 'Ð'
+char_2 = ' '  # space
+
+print(ord(char_1))
+print(ord(char_2))
+
+# Demonstrating the chr() function.
+
+print(chr(197))
+print(chr(205))
+
+# Indexing strings.
+
+the_string = 'silly walks'
+
+for ix in range(len(the_string)):
+    print(the_string[ix], end=' ')
+
+print()
+"""
+# Slices
+
+alpha = "abdefg"
+
+print(alpha[1:3])
+print(alpha[3:])
+print(alpha[:3])
+print(alpha[3:-2])
+print(alpha[-3:4])
+print(alpha[::2])
+print(alpha[1::2])
