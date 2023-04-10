e7abcc21799fb7ab169527e1c4a1eabf0446fb4c
Catalin <stirbuc@icloud.com>
2023-04-10 18:12:41 +0300

    Friday Coding New File:     No Memory.py

---
+++ No Memory.py
@@ -0,0 +1,13 @@
+"""fruits = ['apple', 'banana', 'orange']
+try:
+    print(fruits[3])
+except LookupError as e:
+    print(f"Error: {e}")
+
+string = 'x'
+try:
+    while True:
+        string = string + string
+        print(len(string))
+except MemoryError:
+    print('This is not funny!')"""
