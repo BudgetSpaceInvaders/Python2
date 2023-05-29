02d0ac66cb946db2d0f26cd6ba0b0d44dbecc868
Catalin <stirbuc@icloud.com>
2023-05-29 18:07:00 +0300

    Monday Coding Homework:     New File:         Carti.py:             Learning Data Bases once more

---
+++ Carti.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+class Books:
+    def __init__(self):
+        self.conn = sqlite3.connect('exemplu2.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS books (
+                                id INTEGER AUTO_INCREMENT PRIMARY KEY,
+                                ISBN TEXT,
+                                Title TEXT,
+                                ApparitionDay TEXT,
+                                Writer TEXT
+                            )''')
+        self.conn.commit()
+
+    def adauga(self, ISBN, Title, ApparitionDay, Writer):
+        self.cursor.execute(f"INSERT INTO books (ISBN, Title, ApparitionDay, Writer) VALUES ('{ISBN}', '{Title}', '{ApparitionDay}', '{Writer}')")
+        self.conn.commit()
+    def show(self):
+        self.cursor.execute("SELECT * FROM books")
+        print(self.cursor.fetchall())
+    def sterge(self, id):
+        self.cursor.execute(f"DELETE FROM books WHERE id = {id}")
+        self.conn.commit()
+    def update(self, id, ISBN, Title, ApparitionDay, Writer):
+        self.cursor.execute(f"UPDATE books SET ISBN = {ISBN}, Title = '{Title}', ApparitionDay = {ApparitionDay}, Writer = {Writer} WHERE id = {id}")
+        self.conn.commit()
+book = Books()
+book.adauga("9786067063028", "Aventurile lui Tom Sawyer", "2016", "Mark Twain")
+book.adauga("HRR973-7923-43-X", "Aventurile lui Huckleberry Finn", "2004", "Mark Twain")
+book.show()
