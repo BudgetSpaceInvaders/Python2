a063cf581dea7124ea951d49f4cf4b3864024d41
Catalin <stirbuc@icloud.com>
2023-07-14 17:14:11 +0300

    Monday Coding Update:     Zoo.py:         Added 3 animals to the zoo

--- Zoo.py
+++ Zoo.py
@@ -39,14 +39,33 @@ class Penguin(Animal):
     def make_sound(self):
         return "HONK!!!"
 
+
 class Zoo:
     def __init__(self):
         self.animals = []
+
     def add_animal(self, animal):
         self.animals.append(animal)
+
     def remove_animal(self, animal):
         self.animals.remove(animal)
+
     def show_animals(self):
         for animal in self.animals:
             print(f"Name: {animal.get_name()}, Age: {animal.get_age()}, Sound: {animal.make_sound()}")
 
+
+lion1 = Lion("Simba", 10, "male")
+lion1.make_sound()
+lion1.get_age()
+lion1.get_name()
+
+elephant1 = Elephant("Dumbo", 15, "2 meters")
+elephant1.make_sound()
+elephant1.get_age()
+elephant1.get_name()
+
+penguin1 = Penguin("Pingu", 13, False)
+penguin1.make_sound()
+penguin1.get_age()
+penguin1.get_name()
