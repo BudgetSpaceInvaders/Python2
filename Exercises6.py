eabdca316f85851f22e32fd803678ecbafeba23a
Catalin <stirbuc@icloud.com>
2023-12-08 18:03:21 +0200

    Friday Coding New files:     Exercises6.py:     2 Commits in one day!!!1!1!1!1 That is rare (Added a protection against TyperError)

--- Exercises6.py
+++ Exercises6.py
@@ -6,11 +6,15 @@ nr_list = [2, 4, 6]
 # The function
 def my_sum(*nr_list):
     final_answer = 0
-    for x in nr_list:
-        # Summing the numbers up
-        final_answer += x
-    # The returning
+    try:
+        for x in nr_list:
+            # Summing the numbers up
+            final_answer += x
+    except TypeError:
+        print("No strings, only integers!")
+        # The returning
     return final_answer
+
 
 
 # The actual output
